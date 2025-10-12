8f01b147e4e5212062298dc71f638cc65104d181
Lia-create-ai <awaliarabiatul60@gmail.com>
2025-10-12 11:02:22 +0800

    done kaaa

--- bin/Dart Dasar/function_as_parameter.dart
+++ bin/Dart Dasar/function_as_parameter.dart
@@ -2,3 +2,12 @@ void sayHello(String name, String Function(String) filter) {
   var filteredName = filter(name);
   print('Hi $filteredName');
 }
+
+String filter(String name) {
+  return name;
+}
+
+void main() {
+  sayHello('Rabiatul', filter);
+  sayHello('Awalia', filter);
+}
